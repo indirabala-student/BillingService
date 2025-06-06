5c965c74d073f46e1664d7f03e3a084a4a159f48
indirabala <ibattula@galaxe.com>
2025-06-06 17:39:12 +0530

    Hard coded inter-service URLs moved to application.properties

--- src/main/java/com/meatsfresh/BillingService/service/VendorOrderAggregatorService.java
+++ src/main/java/com/meatsfresh/BillingService/service/VendorOrderAggregatorService.java
@@ -1,8 +1,8 @@
 package com.meatsfresh.BillingService.service;
 
+import com.meatsfresh.BillingService.config.ServiceUrlProperties;
 import com.meatsfresh.BillingService.dto.OrderDTO;
 import com.meatsfresh.BillingService.dto.VendorDTO;
-import com.meatsfresh.BillingService.dto.VendorOrderDTO;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -10,7 +10,6 @@ import org.springframework.web.util.UriComponentsBuilder;
 
 import java.time.LocalDate;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -18,14 +17,12 @@ public class VendorOrderAggregatorService {
 
     private final WebClient.Builder webClientBuilder;
 
-    private static final String VENDOR_SERVICE_BASE_URL="http://localhost:8081/api/vendors";
-
-    private static final String ORDER_SERVICE_BASE_URL = "http://localhost:8080/api/orders";
+    private final ServiceUrlProperties serviceUrlProperties;
 
     public List<VendorDTO> getAllVendors() {
         return webClientBuilder.build()
                 .get()
-                .uri(VENDOR_SERVICE_BASE_URL)
+                .uri(serviceUrlProperties.getVendorBaseUrl())
                 .retrieve()
                 .bodyToFlux(VendorDTO.class)
                 .collectList()
@@ -34,8 +31,7 @@ public class VendorOrderAggregatorService {
 
     public List<OrderDTO> getOrdersByVendorWithDateRange(Long vendorId, LocalDate start, LocalDate end){
 
-        String url = UriComponentsBuilder
-                .fromHttpUrl(ORDER_SERVICE_BASE_URL)
+        String url = UriComponentsBuilder.fromUriString(serviceUrlProperties.getOrderBaseUrl())
                 .path("/vendor/{vendorId}/filter")
                 .queryParam("start", start)
                 .queryParam("end", end)
